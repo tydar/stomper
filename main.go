444621a5f5204f496fa15d962546da204a9d7773
Tyler Darnell <tyler.a.darnell@gmail.com>
2021-11-15 17:00:26 -0600

    move frames files into main package

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "reflect"
-    "github.com/tydar/stomper/frames"
+	"fmt"
+	"reflect"
 )
 
 func main() {
-    f, err := frames.ParseFrame("SEND\n\n\000")
-    f2 := frames.Frame{Command: "SEND", Headers: make(map[string]string), Body: ""}
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(reflect.DeepEqual(f, f2))
+	f, err := ParseFrame("SEND\n\n\000")
+	f2 := Frame{Command: "SEND", Headers: make(map[string]string), Body: ""}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(reflect.DeepEqual(f, f2))
 }
